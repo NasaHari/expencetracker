03bc4b6de5a407ebe6a152d1d3101179a326ffd8
NasaHari <56309180+NasaHari@users.noreply.github.com>
2023-08-03 13:03:31 +0530

    working css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import { GlobalProvider } from "./context/GlobalState";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
